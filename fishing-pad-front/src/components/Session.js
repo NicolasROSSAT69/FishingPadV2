d1e7bc8c0fbfcf03bfb1b9ef65c9994bc63256ce
nicolas <nicolas.rossat@hotmail.fr>
2022-04-17 11:44:48 +0200

    Ajout filtre des session par lieu

--- fishing-pad-front/src/components/Session.js
+++ fishing-pad-front/src/components/Session.js
@@ -7,7 +7,9 @@ const config = require('../config.json');
 const Session = () => {
 
     const [session, setsession] = useState([]);
-
+    const [selectedRadio, setSelectedValue] = useState('');
+    //const radio = ["Africa", "America", "Asia", "Europe", "Oceania"];
+    const [radio, setradio] = useState([]);
 
     useEffect(() => {
 
@@ -25,15 +27,41 @@ const Session = () => {
                     'Authorization': 'Token ' + token
                 }
             }
-            axios.get(config.api_url + 'session/' + idu, configu).then((res) => setsession(res.data));
+            axios.get(config.api_url + 'session/' + idu, configu).then((res) => {
+
+                setsession(res.data);
+
+                const arraytemp = res.data.map(lieu => lieu.lieu);
+                const arrayfin = arraytemp.filter((val, id, array) => array.indexOf(val) === id);
+                setradio(arrayfin);
+
+            });
+
         }
 
     }, []);
     return (
         <div className='container'>
             <ModalSessionAdd />
+            {radio && (
+                <div className='filtre'>
+                    <ul className="radio-container">
+                        {radio.map((lieu) => (
+                            <li>
+                                <input type="radio" id={lieu} name="lieuRadio" checked={lieu === selectedRadio} onChange={(e) => setSelectedValue(e.target.id)} />
+                                <label htmlFor={lieu}>{lieu}</label>
+                            </li>
+                        ))}
+                    </ul>
+                    {selectedRadio && (
+                        <button onClick={() => setSelectedValue("")}>Annuler le filtre</button>
+                    )}
+                </div>
+            )}
+
             <div className='row row-cols-1 row-cols-md-4 g-4'>
                 {session
+                    .filter((session) => session.lieu.includes(selectedRadio))
                     .map((session, index) => (
                         <Cards key={index} session={session} />
                     ))}
